f5aa9e6ce1f3ba8ee99c75ebd27533dffe5ae4a3
hengkysteen <hengkysteen@gmail.com>
2021-02-11 05:11:30 +0700

    remove api-key

--- lib/repos/repo_url.dart
+++ lib/repos/repo_url.dart
@@ -1,4 +1,4 @@
 class RepoUrl {
   static const news = 'https://newsapi.org/v2/everything?domains=bbc.co.uk';
-  static const apiKey = '94007160ad314a1ea19c636789d87745';
+  static const apiKey = 'API_KEY';
 }
